4de6fe7423bdb0ad9243d48b423c4ece1c0d942c
aurelienbrethes <aurelienbrethes@gmail.com>
2021-12-11 17:44:58 +0100

    responsive OK

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -14,21 +14,21 @@ function Project({titre, tech, image, description, link }) {
     }  
 
   return (
-    <div className="project_container">
-        <div className="mini_card">
-            <h3>{titre}</h3>
-            <img src={image} alt="logo du projet" />
-            <button type="button" onClick={() => openModal()}>En savoir plus</button>
-        </div>        
-        <div className={showModal ? "modal" : "displayNone"}>
-            <Modal
-            titre={titre}
-            image={image}
-            tech={tech}
-            description={description}
-            setShowModal={setShowModal}
-            link={link}
-            />
+    <div>
+            <div className="mini_card">
+                <h3>{titre}</h3>
+                <img src={image} alt="logo du projet" />
+                <button type="button" onClick={() => openModal()}>En savoir plus</button>
+            </div>  
+            <div className={showModal ? "modal" : "displayNone"}>
+                <Modal
+                titre={titre}
+                image={image}
+                tech={tech}
+                description={description}
+                setShowModal={setShowModal}
+                link={link}
+                />
         </div>        
     </div>
   );
