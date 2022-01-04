36acd9cb621488d05dc719cd716c90b6178d9e4d
aurelienbrethes <aurelienbrethes@gmail.com>
2022-01-04 16:40:12 +0100

    update project texts

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -11,12 +11,6 @@ const Modal = ({titre, image, description, tech, setShowModal, link, }) => {
         item.stopPropagation(item);
     };
 
-    let cors = "displayNone";
-
-    if( titre === "Blind Crash Test"){
-        cors = ""
-    }
-
     return(
         <div className = "close_modal" role="button" onClick={() => handleParentsClick() }>
             <div className="modal_container" onClick={(e) => handleChildClick(e)}>            
@@ -25,9 +19,6 @@ const Modal = ({titre, image, description, tech, setShowModal, link, }) => {
                     <img src={image} id="logo_project" alt="logo du projet"/>
                 </a>
                 <p>{description}</p>
-                <div className={cors}>
-                    <a target="_blank" rel="noopener noreferrer" href="https://cors-anywhere.herokuapp.com/corsdemo">Lien d'activation</a>
-                </div>
                 <h3>Tech utilisées</h3>
                 <p>{tech}</p>
                 <img src={cross} alt="cross" id="cross" onClick={() => setShowModal(false)} />
