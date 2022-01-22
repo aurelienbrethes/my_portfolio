136b9b0251e91c85f6b119c761b4d69f3677fdad
aurelienbrethes <aurelienbrethes@gmail.com>
2022-01-22 17:46:43 +0100

    add 2 projects and update display

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,7 @@
 import './modal.css';
 import cross from '../assets/cross.png';
 
-const Modal = ({titre, image, description, tech, setShowModal, link, }) => {
+const Modal = ({titre, image, description, tech, setShowModal, link, github }) => {
 
     const handleParentsClick = () => {
         setShowModal(false)
@@ -12,18 +12,31 @@ const Modal = ({titre, image, description, tech, setShowModal, link, }) => {
     };
 
     return(
-        <div className = "close_modal" role="button" onClick={() => handleParentsClick() }>
-            <div className="modal_container" onClick={(e) => handleChildClick(e)}>            
+        <main className = "close_modal" role="button" onClick={() => handleParentsClick() }>
+            <section className="modal_container" onClick={(e) => handleChildClick(e)}>            
                 <h3>{titre}</h3>
-                <a href={link} target="_blank" rel="noopener noreferrer">
-                    <img src={image} id="logo_project" alt="logo du projet"/>
-                </a>
-                <p>{description}</p>
-                <h3>Tech utilisées</h3>
-                <p>{tech}</p>
+                <div className='aside_modal_container'>
+                    <aside className="aside_modal">
+                        <a href={link} target="_blank" rel="noopener noreferrer">
+                            <img src={image} id="logo_project" alt="logo du projet"/>
+                        </a>
+                        <h4>Techs utilisées</h4>
+                        <p>{tech}</p>
+                    </aside>
+                    <aside className="aside_modal">
+                        <h4>Description</h4>
+                        <p>{description}</p>
+                        {github[0] && 
+                        <h4>Lien Github</h4> }                       
+                        {github[0] && 
+                        <a href={github[0]}>Front : Cliquer ici</a> }
+                        {github[1] && 
+                        <a href={github[1]}>Back : Cliquer ici</a> }                      
+                    </aside>
+                </div>           
                 <img src={cross} alt="cross" id="cross" onClick={() => setShowModal(false)} />
-            </div>
-        </div>
+            </section>
+        </main>
     )
 }
 
